805e2f007486a5fee359d1ce5200c4a02980f04f
haiku-san <e.andaloussi.brahim@gmail.com>
2022-12-14 14:59:52 +0100

    connected json data to product page

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,10 +1,29 @@
 import { useParams } from "react-router-dom";
+import data from '../utils/logements.json'
+
+
+export function findLocationById(data, locationId) {
+    // let foundLocation = Object.values(data).find(location => location.id === locationId);
+    let foundLocation;
+    for(let location of data) {
+        // console.log(location)
+        if(location.id === locationId) {
+            foundLocation = location;
+        }
+    }
+    return foundLocation;
+}
 
 export default function Post() {
     const {id} = useParams()
+    console.log(data)
+    let currentLocation = findLocationById(data, id)
+    console.log(currentLocation)
+
     return (
         <div>
             {id}
+            {currentLocation.id}
         </div>
     )
 }

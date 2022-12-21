b11486f4e330477ec1eda57bb92cdb9d6b0a8c90
haiku-san <e.andaloussi.brahim@gmail.com>
2022-12-21 14:56:37 +0100

    added link to the homepage buttons

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,10 +4,12 @@ import logo from '../../assets/images/logo_kasa_512w.png'
 export default function Header() {
     return (
         <header className="header">
-            <img src={logo} alt="Logo Kasa" className="header__logo" />
+            <Link to="/">
+                <img src={logo} alt="Logo Kasa" className="header__logo" />
+            </Link>
             <menu className="menu">
                 <li>
-                    <Link to="#">Accueil</Link>
+                    <Link to="/">Accueil</Link>
                 </li>
                 <li>
                     <Link to="#">A propos</Link>

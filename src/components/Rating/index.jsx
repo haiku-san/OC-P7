c523751a870fc668f6edeebcf8e528a0ad1d071c
haiku-san <e.andaloussi.brahim@gmail.com>
2022-12-21 13:11:26 +0100

    added other elements on product's page

---
+++ src/components/Rating/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import greyStarIcon from '../../assets/images/star_rate-24px.png'
+import redStarIcon from '../../assets/images/star_rate-24px_red.png'
+function Rating({ rating }) {
+    return (
+        <div>
+            <ul>
+                {[...Array(Number(rating))].map((star, i) => (
+                    <li key={i}>
+                        <img
+                            src={redStarIcon}
+                            alt="star icon"
+                            className="starIcon"
+                        />
+                    </li>
+                ))}
+                {[...Array(5 - Number(rating))].map((star, i) => (
+                    <li key={i}>
+                        <img
+                            src={greyStarIcon}
+                            alt="star icon"
+                            className="starIcon"
+                        />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Rating
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom'
 import DropDown from '../components/DropDown'
 import data from '../utils/logements.json'
 import Carousel from '../components/Carousel/index'
+import Rating from '../components/Rating/index'
 
 function findLocationById(data, locationId) {
     let foundLocation = Object.values(data).find(
@@ -16,11 +17,22 @@ export default function Post() {
     let currentLocation = findLocationById(data, id)
     console.log(currentLocation)
 
-    const images = currentLocation.pictures
-
     return (
         <div>
-            <Carousel images={images} />
+            <Carousel images={currentLocation.pictures} />
+            <h1>{currentLocation.title}</h1>
+            <h2>{currentLocation.location}</h2>
+            <ul>
+                {currentLocation.tags.map((tag, i) => (
+                    <li key={i}>{tag}</li>
+                ))}
+            </ul>
+            <h2>{currentLocation.host.name}</h2>
+            <img
+                src={currentLocation.host.picture}
+                alt={'Photo de profil de ' + currentLocation.host.name}
+            />
+            <Rating rating={currentLocation.rating} />
             <DropDown
                 title="Description"
                 content={currentLocation.description}

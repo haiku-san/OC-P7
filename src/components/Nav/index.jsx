eb6d08d6c21d5752159973326d1a03fb57cec7bb
haiku-san <e.andaloussi.brahim@gmail.com>
2022-12-23 20:44:20 +0100

    adapted website for mobiles and tablets

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -2,7 +2,7 @@ export default function Nav({ title, image }) {
     return (
         <nav className="banner">
             <img src={image} alt={title} className="banner__image" />
-            <h1 className="banner_title">{title}</h1>
+            <h1 className="banner__title">{title}</h1>
         </nav>
     )
 }
